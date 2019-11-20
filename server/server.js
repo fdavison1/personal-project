1bc642c2fc33a98fea7105604350b047cd551a45
fdavison1 <fred.noreply@gmail.com>
2019-11-20 09:38:03 -0700

    server - initial set up

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+// const session = require('express-session')
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
+
+const app = express()
+
+//middleware
+app.use(express.json())
+//app.use(session...)
+
+//endpoints
+
+//listening
+app.listen(SERVER_PORT, () => console.log(`port ${ SERVER_PORT } is working?`))
