7a158a8a44c4ca54809a9f057687f9fe7c173e8e
fdavison1 <fred.noreply@gmail.com>
2019-12-04 09:50:59 -0700

    setupTests.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { shallow, mount, configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import '../setupTests'
 import App from '../App'
-
-configure({ adapter: new Adapter() })
 
 describe('App.js tests', () => {
   it('renders', () => {
